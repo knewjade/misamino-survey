fa93adc0fbb3f01382bf291645268fa4a2571131
knewjade <knewjade@gmail.com>
2019-10-12 13:55:13 +0900

    Survey gamepool

--- test/suevey/test_gamepool.cpp
+++ test/suevey/test_gamepool.cpp
@@ -121,4 +121,162 @@ namespace survey {
                             "44|1111111111\n"
         );
     }
+
+    TEST_F(GeameFieldTest, kickI1) {
+        auto field = GameField();
+        field.reset(10, 20);
+
+        field.setBlock(0, 19);
+        field.setBlock(1, 19);
+        field.setBlock(7, 19);
+        field.setBlock(8, 19);
+        field.setBlock(9, 19);
+
+        field.setBlock(0, 18);
+        field.setBlock(1, 18);
+        field.setBlock(2, 18);
+        field.setBlock(5, 18);
+        field.setBlock(6, 18);
+        field.setBlock(7, 18);
+        field.setBlock(8, 18);
+        field.setBlock(9, 18);
+
+        auto ss = std::stringstream{};
+        ss << field;
+        EXPECT_EQ(ss.str(), "\n"
+                            "21|0000000000\n"
+                            "22|0000000000\n"
+                            "23|0000000000\n"
+                            "24|0000000000\n"
+                            "25|0000000000\n"
+                            "26|0000000000\n"
+                            "27|0000000000\n"
+                            "28|0000000000\n"
+                            "29|0000000000\n"
+                            "30|0000000000\n"
+                            "31|0000000000\n"
+                            "32|0000000000\n"
+                            "33|0000000000\n"
+                            "34|0000000000\n"
+                            "35|0000000000\n"
+                            "36|0000000000\n"
+                            "37|0000000000\n"
+                            "38|0000000000\n"
+                            "39|1111100111\n"
+                            "40|1110000011\n"
+                            "41|1111111111\n"
+                            "42|1111111111\n"
+                            "43|1111111111\n"
+                            "44|1111111111\n"
+        );
+
+
+        // field.paste(2, 17, getGem(GEMTYPE_I, Rotation::Left));
+        //
+        // "37|0000001000\n"
+        // "38|0000001000\n"
+        // "39|1111101111\n"
+        // "40|1110001011\n"
+
+        int x = 2;
+        int y = 17;
+        auto gem = getGem(GEMTYPE_I, Rotation::Reverse);
+        EXPECT_TRUE(field.wallkickTest(x, y, gem, 1));
+    }
+
+    TEST_F(GeameFieldTest, kickI2) {
+        auto field = GameField();
+        field.reset(10, 20);
+
+        field.setBlock(0, 19);
+        field.setBlock(1, 19);
+        field.setBlock(2, 19);
+        field.setBlock(8, 19);
+        field.setBlock(9, 19);
+
+        field.setBlock(0, 18);
+        field.setBlock(1, 18);
+        field.setBlock(2, 18);
+        field.setBlock(3, 18);
+        field.setBlock(4, 18);
+        field.setBlock(7, 18);
+        field.setBlock(8, 18);
+        field.setBlock(9, 18);
+
+        auto ss = std::stringstream{};
+        ss << field;
+        EXPECT_EQ(ss.str(), "\n"
+                            "21|0000000000\n"
+                            "22|0000000000\n"
+                            "23|0000000000\n"
+                            "24|0000000000\n"
+                            "25|0000000000\n"
+                            "26|0000000000\n"
+                            "27|0000000000\n"
+                            "28|0000000000\n"
+                            "29|0000000000\n"
+                            "30|0000000000\n"
+                            "31|0000000000\n"
+                            "32|0000000000\n"
+                            "33|0000000000\n"
+                            "34|0000000000\n"
+                            "35|0000000000\n"
+                            "36|0000000000\n"
+                            "37|0000000000\n"
+                            "38|0000000000\n"
+                            "39|1110011111\n"
+                            "40|1100000111\n"
+                            "41|1111111111\n"
+                            "42|1111111111\n"
+                            "43|1111111111\n"
+                            "44|1111111111\n"
+        );
+
+
+        // field.paste(2, 17, getGem(GEMTYPE_I, Rotation::Left));
+        //
+        // "37|0001000000\n"
+        // "38|0001000000\n"
+        // "39|1111011111\n"
+        // "40|1101000111\n"
+
+//        field.paste(5, 17, getGem(GEMTYPE_I, Rotation::Left));
+//        std::cout << field << std::endl;
+//
+        int x = 5;
+        int y = 17;
+        auto gem = getGem(GEMTYPE_I, Rotation::Spawn);
+        EXPECT_TRUE(field.wallkickTest(x, y, gem, 0));
+    }
+
+    TEST_F(GeameFieldTest, getAttack) {
+        auto field = GameField();
+        field.reset(10, 20);
+        field.setBlock(0, 19);  // パフェにならないようにする
+
+        field.combo = 0;
+        field.b2b = 0;
+
+        EXPECT_EQ(field.getAttack(0, 0), 0);
+        EXPECT_EQ(field.getAttack(1, 0), 0);  // Single
+        EXPECT_EQ(field.getAttack(2, 0), 1);  // Double
+        EXPECT_EQ(field.getAttack(3, 0), 2);  // Triple
+        EXPECT_EQ(field.getAttack(4, 0), 4);  // Tetris
+
+        EXPECT_EQ(field.getAttack(1, 1), 2);  // TSS
+        EXPECT_EQ(field.getAttack(2, 1), 4);  // TSD
+        EXPECT_EQ(field.getAttack(3, 1), 6);  // TST
+
+        EXPECT_EQ(field.getAttack(1, 2), 0);  // TSM
+        EXPECT_EQ(field.getAttack(2, 2), 4);  // TSM Double  // ぷよテトでは1
+
+        field.b2b = 1;
+
+        EXPECT_EQ(field.getAttack(1, 1), 3);  // B2B TSS
+        EXPECT_EQ(field.getAttack(2, 1), 5);  // B2B TSD
+        EXPECT_EQ(field.getAttack(3, 1), 7);  // B2B TST
+
+        EXPECT_EQ(field.getAttack(1, 2), 1);  // B2B TSM
+        EXPECT_EQ(field.getAttack(2, 2), 5);  // B2B TSM Double  // ぷよテトでは2
+    }
 }
