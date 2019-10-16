4573568d4df27b6d53f810fac3a84f8d4636c010
knewjade <knewjade@gmail.com>
2019-10-16 23:17:29 +0900

    Survey tetris ai

--- test/suevey/test_tetris_ai.cpp
+++ test/suevey/test_tetris_ai.cpp
@@ -27,10 +27,29 @@ namespace survey {
         long long clearScore = 0;
         double avg_height = 0;
         auto param = AI_Param{};
+        param.tspin = 1;
 
         auto field = GameField();
         field.reset(10, 20);
         field.setBlock(0, 19);
+        field.setBlock(1, 19);
+        field.setBlock(2, 19);
+        field.setBlock(3, 19);
+        field.setBlock(4, 19);
+        field.setBlock(5, 19);
+        field.setBlock(7, 19);
+        field.setBlock(8, 19);
+        field.setBlock(9, 19);
+
+        field.setBlock(0, 18);
+        field.setBlock(1, 18);
+        field.setBlock(2, 18);
+        field.setBlock(3, 18);
+        field.setBlock(4, 18);
+        field.setBlock(8, 18);
+        field.setBlock(9, 18);
+
+        std::cout << field << std::endl;
 
         int score = Evaluate(
                clearScore, avg_height, param, field, field, GEMTYPE_I, 0,
